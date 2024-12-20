f4ac8568ca90385f32179f761d2ff62925aa3d44
123 <152121398+Grimm57@users.noreply.github.com>
2024-12-20 21:44:47 +0800

    Update selfcleaner.v

--- kitchenHood/mode/selfcleaner.v
+++ kitchenHood/mode/selfcleaner.v
@@ -23,54 +23,37 @@
 module selfcleaner(
     input clk,
     input rst,
-    input [2:0] mode_state,      
-    input menu_btn,
-    input self_clean_btn,
-    output reg led_self_clean,
+    input [2:0] modestate,
     output [7:0] digit1,
     output [7:0] digit2,
     output [7:0] tube_sel
 );
-    wire clk_1hz;
-    
-    ClockDivider1Hz ClockDivider1Hz(
-        .clk(clk),
-        .rst(rst),
-        .clk_out(clk_1hz)
-    );
-    
-
-    wire [7:0] clean_digit1, clean_digit2, clean_tube_sel;
-    reg [7:0] digit1_reg, digit2_reg, tube_sel_reg;
 
     reg [5:0] sec = 0;          //倒计时秒数
     reg [5:0] minute = 3;       //倒计时分钟数
+    integer counter=0;
 
-
-    //自清洁模式
-    always @(posedge clk_1hz or negedge rst) begin
+    // 自清洁模式的倒计时
+    always @(posedge clk or negedge rst) begin
         if (!rst) begin
             minute <= 3;  // 复位时分钟数重新设置为 3
             sec <= 0;     // 复位时秒数重新设置为 0
+            counter <= 0; // 计数器复位
         end else begin
-            if (mode_state == 3'b100) begin
-                // 自清洁模式开始时进行倒计时
-                if (sec == 0 && minute > 0) begin
-                    sec <= 59;     // 秒数归零后重置为 59
-                    minute <= minute - 1; // 分钟数减 1
-                end else if (sec > 0) begin
-                    sec <= sec - 1; // 每秒倒计时减 1
+            if (modestate == 3'b100) begin  // 当处于自清洁模式时开始倒计时
+                // 计数器逻辑：每当系统时钟的计数器到达 100,000,000 时，表示 1 秒
+                if (counter == 99_999_999) begin
+                    counter <= 0;  // 计数器重置
+                    if (sec == 0 && minute > 0) begin
+                        sec <= 59;  // 秒数归零后重置为 59
+                        minute <= minute - 1;  // 分钟数减 1
+                    end else if (sec > 0) begin
+                        sec <= sec - 1;  // 每秒钟减 1
+                    end
+                end else begin
+                    counter <= counter + 1;  // 每个时钟周期计数 +1
                 end
             end
-        end
-
-    end
-
-    always @(minute or sec) begin
-        if (minute == 0 && sec == 0) begin
-            led_self_clean <= 1;  // 倒计时结束，设置指示器为1
-        end else begin
-            led_self_clean <= 0;  // 倒计时未结束，保持指示器为0
         end
     end
 
@@ -98,11 +81,6 @@ module selfcleaner(
         .tube_sel(tube_sel)
     );
   
-
-    // 输出信号
-    assign digit1 = digit1_reg;
-    assign digit2 = digit2_reg;
-    assign tube_sel = tube_sel_reg;
 
 endmodule
 
